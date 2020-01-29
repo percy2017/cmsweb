b1790ca0b033f0582a3ec9a9f8b70d816d879be9
jaiko92 <jaiko92.rm@gmail.com>
2020-01-29 16:29:05 -0400

    1.8 se cambio el perfil Frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,89 +1,131 @@
 @extends('layouts.app')
 @section('css')
-    <style>
-        .form-elegant .font-small {
-        font-size: 0.8rem; }
-
-        .form-elegant .z-depth-1a {
-        -webkit-box-shadow: 0 2px 5px 0 rgba(55, 161, 255, 0.26), 0 4px 12px 0 rgba(121, 155, 254, 0.25);
-        box-shadow: 0 2px 5px 0 rgba(55, 161, 255, 0.26), 0 4px 12px 0 rgba(121, 155, 254, 0.25); }
-
-        .form-elegant .z-depth-1-half,
-        .form-elegant .btn:hover {
-        -webkit-box-shadow: 0 5px 11px 0 rgba(85, 182, 255, 0.28), 0 4px 15px 0 rgba(36, 133, 255, 0.15);
-        box-shadow: 0 5px 11px 0 rgba(85, 182, 255, 0.28), 0 4px 15px 0 rgba(36, 133, 255, 0.15); }
-    </style>
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center py-4">
-        <div class="col-md-9">
-            {{-- <div class="card">
-                <div class="card-header">
-                    <h4 class="text-center">Mi Perfil</h4>
-                </div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <div class="form-group row">
-                        <img src="{{ Voyager::Image(Auth::user()->avatar) }}" alt="..." class="img-thumbnail col-md-6">
-                        <address for="rol" class="col-md-6 col-form-label text-md-left">
-                            <p><strong>Nombre: </strong>{{ Auth::user()->name }}</p>
-                            <p><strong>Rol: </strong>{{ Auth::user()->role->name }}</p>
-                            <p><strong>Usuario: </strong>{{ Auth::user()->email }}</p>
-                            <p><strong>Registro: </strong>{{ Auth::user()->created_at }}</p>
-                            <hr />
-                            @if(Auth::user()->role->id == 1)
-                                <a class="btn btn-primary" href="{{ route('voyager.dashboard') }}">Ir a Panel</a>
-                            @endif
-                            <a class="btn btn-default" href="/">Volver</a>
-                        </address>
-                    </div>
-                </div>
-            </div> --}}
-
-                <section class="form-elegant">
-
-            <!--Form without header-->
-            <div class="card">
-
-                <div class="card-body mx-4">
-
-                <!--Header-->
-                <div class="text-center">
-                    <h3 class="dark-grey-text mb-5"><strong>Mi Perfil</strong></h3>
-                </div>
-                <hr />
-                
-                <div class="form-group row">
-                    <img src="{{ Voyager::Image(Auth::user()->avatar) }}" alt="..." class="img-thumbnail col-md-6">
-                    <address for="rol" class="col-md-6 col-form-label text-md-left">
-                        <p><strong>Nombre: </strong>{{ Auth::user()->name }}</p>
-                        <p><strong>Rol: </strong>{{ Auth::user()->role->name }}</p>
-                        <p><strong>Usuario: </strong>{{ Auth::user()->email }}</p>
-                        <p><strong>Registro: </strong>{{ Auth::user()->created_at }}</p>
-                        <hr />
-                        @if(Auth::user()->role->id == 1)
-                            <a class="btn btn-primary" href="{{ route('voyager.dashboard') }}">Ir a Panel</a>
-                        @endif
-                        <a class="btn" href="/">Volver al Home</a>
-                    </address>
-                </div>
-               
-
-
-            </div>
-            <!--/Form without header-->
-
-        </section>
+<div class="container-fluid">
+    <!-- Section: Edit Account -->
+    <section class="section py-5">
+      <!-- First row -->
+      <div class="row">
+        @if (session('status'))
+        <div class="alert alert-success" role="alert">
+          {{ session('status') }}
         </div>
-
-    
-
-    </div>
-</div>
+        @endif
+        <!-- First column -->
+        <div class="col-lg-4 mb-4">
+  
+          <!-- Card -->
+          <div class="card card-cascade narrower">
+  
+            <!-- Card image -->
+            <div class="view view-cascade gradient-card-header blue-gradient mdb-color lighten-3">
+              <h5 class="mb-0 font-weight-bold">Foto</h5>
+            </div>
+            <!-- Card image -->
+  
+            <!-- Card content -->
+            <div class="card-body card-body-cascade text-center">
+              <img src="{{ Voyager::Image(Auth::user()->avatar) }}" alt="User Photo" class="z-depth-1 mb-3 mx-auto" />
+  
+              <p class="text-muted"><small>Bienvenido al mejor CmsWeb</small></p>
+              <div class="row flex-center">
+                @if(Auth::user()->role->id == 1)
+                <a class="btn btn-info btn-rounded btn-sm" href="{{ route('voyager.dashboard') }}">Ir a Panel</a>
+                @endif
+                <a class="btn btn-danger btn-rounded btn-sm" href="/">Volver</a>
+              </div>
+            </div>
+            <!-- Card content -->
+  
+          </div>
+          <!-- Card -->
+  
+        </div>
+        <!-- First column -->
+  
+        <!-- Second column -->
+        <div class="col-lg-8 mb-4">
+  
+          <!-- Card -->
+          <div class="card card-cascade narrower">
+  
+            <!-- Card image -->
+            <div class="view view-cascade gradient-card-header  blue-gradient mdb-color lighten-3">
+              <h5 class="mb-0 font-weight-bold">Mi Perfil</h5>
+            </div>
+            <!-- Card image -->
+  
+            <!-- Card content -->
+            <div class="card-body card-body-cascade text-center">
+  
+              <!-- Edit Form -->
+              <form>
+  
+                <!-- First row -->
+                <div class="row">
+                  <!-- First column -->
+                  <div class="col-md-6">
+                    <div class="md-form mb-0">
+                      <input type="text" id="form1" class="form-control" value="{{ Auth::user()->name }}" disabled>
+                      <label style="font-size: 1.1rem;" for="form1" data-error="wrong" data-success="right">Nombres
+                        :</label>
+  
+                    </div>
+                  </div>
+  
+                  <!-- Second column -->
+                  <div class="col-md-6">
+                    <div class="md-form mb-0">
+                      <input type="text" id="form1" class="form-control validate" value="{{ Auth::user()->role->name }}"
+                        disabled>
+                      <label style="font-size: 1.1rem;" for="form1" data-error="wrong" data-success="right">Rol :</label>
+                    </div>
+                  </div>
+                </div>
+                <!-- First row -->
+  
+                <!-- Second row -->
+                <div class="row">
+  
+                  <!-- First column -->
+                  <div class="col-md-6">
+                    <div class="md-form mb-0">
+                      <input type="text" id="form1" class="form-control validate" value="{{ Auth::user()->email }}"
+                        disabled>
+                      <label style="font-size: 1.1rem;" for="form1" data-error="wrong" data-success="right">Email
+                        :</label>
+                    </div>
+                  </div>
+                  <!-- Second column -->
+  
+                  <div class="col-md-6">
+                    <div class="md-form mb-0">
+                      <input type="text" id="form1" class="form-control validate" value="{{ Auth::user()->created_at }}"
+                        disabled>
+                      <label style="font-size: 1.1rem;" for="form1" data-error="wrong" data-success="right">Registro
+                        :</label>
+                    </div>
+                  </div>
+                </div>
+                <!-- Second row -->
+  
+              </form>
+              <!-- Edit Form -->
+  
+            </div>
+            <!-- Card content -->
+  
+          </div>
+          <!-- Card -->
+  
+        </div>
+        <!-- Second column -->
+  
+      </div>
+      <!-- First row -->
+    </section>
+    <!-- Section: Edit Account -->
+  </div>
 @endsection
