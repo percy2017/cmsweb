439c35d9548f6467bbd04c3ed274c3597e81f25e
jaiko92 <jaiko92.rm@gmail.com>
2020-02-19 22:44:27 -0400

    add edit modal

---
+++ resources/views/vendor/streaming/table.blade.php
@@ -0,0 +1,27 @@
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre Completo</th>
+            <th scope="col">Telefono</th>
+            <th scope="col">Fecha de Inicio</th>
+            <th scope="col">acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach ($viewProfile as $item)
+        <tr>
+            <th scope="row">{{ $item->id}}</th>
+            <td>{{ $item->fullname }} </td>
+            <td>{{ $item->phone }}</td>
+            <td>{{ $item->startdate }} </td>
+            <td>
+            <a href="#" onclick="s_edit('{{ route('s_edit', $item->id) }}')" class="btn btn-warning">
+                    <i class="voyager-pen"></i>
+                </a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
